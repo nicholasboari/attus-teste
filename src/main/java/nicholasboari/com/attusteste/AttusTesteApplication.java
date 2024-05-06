62d65d335b5549be6dc41dbe1e30005943bdfd9f
nicholasboari <nicholasboari@gmail.com>
2024-05-06 09:20:35 -0300

    feat: initial commit

---
+++ src/main/java/nicholasboari/com/attusteste/AttusTesteApplication.java
@@ -0,0 +1,13 @@
+package nicholasboari.com.attusteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttusTesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttusTesteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nicholasboari/com/attusteste/AttusTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package nicholasboari.com.attusteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttusTesteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
